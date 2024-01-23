66f31119b86da92de6c29f24051d6528b9ee1eb1
bodoganabel <bodoganabel@gmail.com>
2024-01-23 12:55:23 +0100

    Create multiple

--- src/gql/product/product.entities.ts
+++ src/gql/product/product.entities.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+
 export const productEntities = `
     type Product {
         _id: ObjectId!
@@ -6,4 +8,16 @@ export const productEntities = `
         producerId: ObjectId!
         producer: Producer!
     }
+
+    input ProductInput {
+        vintage: String!
+        name: String!
+        producerId: ObjectId!
+    }
 `;
+
+export interface IProductInput {
+    vintage: string;
+    name: string;
+    producerId: ObjectId;
+}
--- src/gql/product/product.resolvers.ts
+++ src/gql/product/product.resolvers.ts
@@ -1,6 +1,7 @@
 import { Collection, ObjectId } from "mongodb";
+import { IProductInput } from "./product.entities";
 
-export function productResolvers(productsCollection: Collection) {
+export function productResolvers(productsCollection: Collection, producerCollection: Collection) {
     return {
         // Fetch a single product by its _id from your database
         async product({ _id }: { _id: string }) {
@@ -19,5 +20,43 @@ export function productResolvers(productsCollection: Collection) {
             }
             return product;
         },
+
+        async createMultipleProducts(productsInput: { products: IProductInput[] }) {
+            const products = productsInput.products;
+
+            console.log('products:');
+            console.log(products);
+            try {
+                const validProducts = [];
+                const invalidProducts = [];
+
+                // Check if producerId exists for each product
+                for (const product of products) {
+                    const producerExists = await producerCollection.findOne({ _id: new ObjectId(product.producerId) });
+                    if (producerExists) {
+                        validProducts.push(product);
+                    } else {
+                        invalidProducts.push(product);
+                        console.error(`Error: Producer not found for product: `, product);
+                    }
+                }
+
+                if (validProducts.length === 0) {
+                    throw new Error('No valid products to insert.');
+                }
+
+                // Insert only valid products
+                const result = await productsCollection.insertMany(validProducts);
+
+                // Fetch the inserted documents using the insertedIds
+                const insertedProducts = await productsCollection.find({
+                    _id: { $in: Object.values(result.insertedIds) }
+                }).toArray();
+
+                return insertedProducts;
+            } catch (error: any) {
+                throw new Error('Error creating multiple products: ' + error.message);
+            }
+        }
     }
 }
